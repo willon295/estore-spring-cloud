815c4841721b6fe3688364da0fdee6002ed25ad0
willon295 <willon295@gmail.com>
2018-07-27 14:16:32 +0800

    update doc

--- code/book-service/src/main/java/com/briup/estore/common/dto/ResponseDTO.java
+++ code/book-service/src/main/java/com/briup/estore/common/dto/ResponseDTO.java
@@ -9,15 +9,17 @@ import lombok.Data;
  */
 @Data
 public class ResponseDTO {
+
     /**
-     * 处理的结果
-     * OK   成功处理
-     * FAILED 处理失败
+     * 状态
+     * OK     成功
+     * FAILED 失败
      */
     private String msg;
 
     /**
-     * 返回的数据
+     * 数据
      */
     private Object data;
+
 }
